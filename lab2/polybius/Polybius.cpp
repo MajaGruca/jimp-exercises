ee248ae4c02a917958420644c8221a399d764e57
Maja <majag000@gmail.com>
2017-03-13 11:59:53 +0100

    Polybius added

--- lab2/polybius/Polybius.cpp
+++ lab2/polybius/Polybius.cpp
@@ -1,10 +1,72 @@
-//
-// Created by ktr on 11.03.2017.
-//
+#include <Polybius.h>
 
-#include "Polybius.h"
-#include <string>
-using namespace std;
+std::string PolybiusCrypt(std::string message)
+{
+    int i,j,k;
+    char letter=97;
+    char pol[5][5];
+    for(i=0;i<5;i++)
+    {
+        for(j=0;j<5;j++)
+        {
+            if(letter==106)
+                letter++;
+            pol[i][j]=letter;
+            letter++;
+        }
+    }
+    std::string crypted;
+    for(i=0;i<message.length();i++)
+    {
+    	if(message[i]==' ')
+    		continue;
+        message[i] = tolower(message[i]);
+        if(message[i]=='j')
+            message[i] = 'i';
+        for(j=0;j<5;j++)
+        {
+            for(k=0;k<5;k++)
+            {
+                if(pol[j][k]==message[i])
+                {
+                    crypted += std::to_string(j+1);
+                    crypted += std::to_string(k+1);
+                    break;
+                }
+            }
+        }
+    }
+    return crypted;
+}
 
-string PolybiusCrypt(string message);
-string PolybiusDecrypt(string crypted);
+std::string PolybiusDecrypt(std::string crypted)
+{
+    int i,j;
+    char letter=97;
+    char pol[5][5];
+    for(i=0;i<5;i++)
+    {
+        for(j=0;j<5;j++)
+        {
+            if(letter==106)
+                letter++;
+            pol[i][j]=letter;
+            letter++;
+        }
+    }
+    std::string decrypted;
+    for(i=0;i<crypted.length();i=i+2)
+    {
+    	int row, column;
+    	for(j=1;j<6;j++)
+    	{
+    		if(j==crypted[i] - '0')
+    			row=j;
+    		if(j==crypted[i+1] - '0')
+    			column=j;
+		}
+		decrypted += pol[row-1][column-1];
+    }
+    return decrypted;
+}
+
--- lab2/polybius/main.cpp
+++ lab2/polybius/main.cpp
@@ -1,11 +1,32 @@
-//
-// Created by ktr on 11.03.2017.
-//
+#include <Polybius.h>
 
-#include "Polybius.h"
-using namespace std;
-
-int main()
+int main(std::string file1, std::string file2, int option)
 {
+    std::ifstream file_to_open(file1);
+    std::ofstream file_to_save(file2, std::ios_base::in | std::ios_base::app);
+    char line[256];
+
+    if(!file_to_open)
+        std::cout << "Nie mozna otworzyc pliku!" << std::endl;
+    if(option==0)
+    {
+        while(!file_to_open.eof())
+        {
+            file_to_open.getline(line,256);
+            file_to_save << PolybiusCrypt(line) << std::endl;
+        }
+
+    }
+    else
+    {
+        while(!file_to_open.eof())
+        {
+            file_to_open.getline(line,256);
+            file_to_save << PolybiusDecrypt(line) << std::endl;
+        }
+    }
+
+    file_to_open.close();
+    file_to_save.close();
     return 0;
 }
