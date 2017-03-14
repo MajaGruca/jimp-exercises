236d8eadde24c8cc518d54d624610079b6ca16ca
Maja <majag000@gmail.com>
2017-03-14 13:11:27 +0100

    XOR full <3

--- lab2/xorcypherbreaker/XorCypherBreaker.cpp
+++ lab2/xorcypherbreaker/XorCypherBreaker.cpp
@@ -15,6 +15,71 @@ string XorCypherBreaker(const vector<char> &cryptogram,
 
 {
         string temp;
+        char key[key_length+1];
+        key[key_length]='\0';
+        char temp_key[key_length];
+        int iterator[key_length];
+        int itt;
+        int match;
+        int match_max = 0;
+        char letter;
+        for(int j=0;j<key_length-1;j++)
+            iterator[j]=97;
+        iterator[key_length-1]=96;
+        while(iterator[0]<=122)
+        {
+            iterator[key_length-1]++;
+            for(int i=key_length-1;i>=0;i--)
+            {
+                if(iterator[i]>122 && i!=0)
+                {
+                    iterator[i-1]++;
+                    iterator[i]=97;
+                }
+            }
+            if(iterator[0]==123)
+            {
+                break;
+            }
+            for(int i=0;i<key_length;i++)
+            {
+                temp_key[i] = (char)(iterator[i]);
+            }
+            match = 0;
+            temp="";
+            itt=0;
+            for (int it : cryptogram)
+            {
+                letter = (char)(temp_key[itt % key_length] xor it);
+                if ((letter >= 97) && (letter <= 122))
+                    temp+=letter;
+                else
+                {
+                    if (temp.length()>key_length-1 && find(dictionary.begin(), dictionary.end(), temp) != dictionary.end())
+                    {
+                        match++;
+                    }
+                    temp="";
+                }
+                itt++;
+            }
+            if(match>match_max)
+            {
+                match_max=match;
+                for(int k=0;k<key_length;k++)
+                {
+                    key[k]=temp_key[k];
+                }
+            }
+        }
+    return key;
+}
+
+
+/*    wersja dla 3 zmiennych
+
+{
+        string temp;
         string key="";
         string temp_key = "   ";
         int itt;
@@ -58,4 +123,4 @@ string XorCypherBreaker(const vector<char> &cryptogram,
             }
         }
         return key;
-}
+}*/
--- lab2/xorcypherbreaker/main.cpp
+++ lab2/xorcypherbreaker/main.cpp
@@ -2,10 +2,7 @@
 // Created by ktr on 13.03.2017.
 //
 
-#include <iostream>
-
 #include "XorCypherBreaker.h"
-using namespace std;
 
 int main()
 {
