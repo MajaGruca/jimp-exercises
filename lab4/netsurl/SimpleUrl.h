75e18500053b86e38f915b0c87523ff90a195481
PaulinaKaplonJimp <paulinakaplon@o2.pl>
2017-03-21 18:16:36 +0100

    na jimp lab3

---
+++ lab4/netsurl/SimpleUrl.h
@@ -0,0 +1,14 @@
+//
+// Created by ktr on 21.03.2017.
+//
+
+#ifndef JIMP_EXERCISES_SIMPLEURL_H
+#define JIMP_EXERCISES_SIMPLEURL_H
+
+
+class SimpleUrl {
+
+};
+
+
+#endif //JIMP_EXERCISES_SIMPLEURL_H
