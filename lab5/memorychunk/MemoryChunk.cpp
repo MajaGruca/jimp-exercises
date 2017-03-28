8d999c428f4e1477bcd0906ae648d3a4da13c1c8
Maj333 <majag000@gmail.com>
2017-03-28 20:14:58 +0200

    Forward

--- lab5/memorychunk/MemoryChunk.cpp
+++ lab5/memorychunk/MemoryChunk.cpp
@@ -3,7 +3,67 @@
 //
 
 #include "MemoryChunk.h"
-namespace memory
+namespace memorychunk
 {
+    MemoryChunk::MemoryChunk(size_t sz) {
 
+        ptr = new int8_t[sz];
+        siz=sz;
+    }
+    MemoryChunk::~MemoryChunk(){
+        delete [] ptr;
+        siz=0;
+    }
+
+    MemoryChunk::MemoryChunk(const MemoryChunk& memorychunk) {
+        ptr = new int8_t[memorychunk.siz];
+        ptr = memorychunk.ptr;
+    }
+
+    MemoryChunk & MemoryChunk::operator=(const MemoryChunk& memorychunk) {
+        if (this == &memorychunk) {
+            return *this;
+        }
+
+        delete[] ptr;
+
+        ptr = new int8_t[memorychunk.siz];
+        ptr = memorychunk.ptr;
+        siz=memorychunk.siz;
+    }
+
+    MemoryChunk::MemoryChunk(MemoryChunk &&xxx) : ptr{nullptr} {
+        std::swap(ptr,xxx.ptr);
+        std::swap(siz,xxx.siz);
+    }
+
+    MemoryChunk & MemoryChunk::operator=(MemoryChunk &&xxx) {
+        if (this == &xxx) {
+            return xxx;
+        }
+        delete[] ptr;
+        ptr = nullptr;
+        siz=0;
+        std::swap(ptr,xxx.ptr);
+        std::swap(siz,xxx.siz);
+    }
+
+
+    int8_t *MemoryChunk::MemoryAt(size_t offset) const
+    {
+        if(offset>=0 && offset<siz)
+            return ptr+offset;
+        else
+            return nullptr;
+    }
+    size_t MemoryChunk::ChunkSize() const
+    {
+        if(ptr)
+        {
+            return siz;
+        }
+        else
+            return 0;
+
+    }
 }
--- lab5/memorychunk/MemoryChunk.h
+++ lab5/memorychunk/MemoryChunk.h
@@ -4,13 +4,23 @@
 
 #ifndef JIMP_EXERCISES_MEMORYCHUNK_H
 #define JIMP_EXERCISES_MEMORYCHUNK_H
-
-namespace memory
+#include <cstdint>
+#include <algorithm>
+namespace memorychunk
 {
-    class MemoryChunk {
-
-
-
+    class MemoryChunk{
+    public:
+        MemoryChunk(size_t sz);
+        ~MemoryChunk();
+        int8_t *MemoryAt(size_t offset) const;
+        size_t ChunkSize() const;
+        MemoryChunk(const MemoryChunk& memorychunk);
+        MemoryChunk & operator=(const MemoryChunk& memorychunk);
+        MemoryChunk & operator=(MemoryChunk &&xxx);
+        MemoryChunk(MemoryChunk &&xxx);
+    private:
+        int8_t *ptr;
+        size_t siz;
     };
 }
 
