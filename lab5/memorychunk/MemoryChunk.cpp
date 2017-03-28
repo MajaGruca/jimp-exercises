ab99cfa710cc3e254aa3ac025512283fe0cdd135
PaulinaKaplonJimp <paulinakaplon@o2.pl>
2017-03-28 18:35:29 +0200

    na jimp lab4

---
+++ lab5/memorychunk/MemoryChunk.cpp
@@ -0,0 +1,9 @@
+//
+// Created by ktr on 28.03.2017.
+//
+
+#include "MemoryChunk.h"
+namespace memory
+{
+
+}
---
+++ lab5/memorychunk/MemoryChunk.h
@@ -0,0 +1,19 @@
+//
+// Created by ktr on 28.03.2017.
+//
+
+#ifndef JIMP_EXERCISES_MEMORYCHUNK_H
+#define JIMP_EXERCISES_MEMORYCHUNK_H
+
+namespace memory
+{
+    class MemoryChunk {
+
+
+
+    };
+}
+
+
+
+#endif //JIMP_EXERCISES_MEMORYCHUNK_H
