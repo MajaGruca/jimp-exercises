04b4e71d2088440bd4e6267c8530760f8e781964
PaulinaKaplonJimp <paulinakaplon@o2.pl>
2017-04-08 12:39:48 +0200

    na jimp lab6

---
+++ lab6/wordcounter/WordCounter.cpp
@@ -0,0 +1,10 @@
+//
+// Created by ktr on 08.04.2017.
+//
+
+#include "WordCounter.h"
+
+namespace datastructures
+{
+
+};
---
+++ lab6/wordcounter/WordCounter.h
@@ -0,0 +1,23 @@
+//
+// Created by ktr on 08.04.2017.
+//
+
+#ifndef JIMP_EXERCISES_WORDCOUNTER_H
+#define JIMP_EXERCISES_WORDCOUNTER_H
+
+namespace datastructures
+{
+    class WordCounter {
+
+    };
+    class Counts {
+
+    };
+    class Word {
+
+    };
+};
+
+
+
+#endif //JIMP_EXERCISES_WORDCOUNTER_H
