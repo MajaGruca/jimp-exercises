5ea76594c24a83417c460f64c83ce8dba2e659aa
Maj333 <majag000@gmail.com>
2017-04-11 18:45:07 +0200

    lab7

--- lab7/arrayfill/ArrayFill.h
+++ lab7/arrayfill/ArrayFill.h
@@ -4,11 +4,23 @@
 
 #ifndef JIMP_EXERCISES_ARRAYFILL_H
 #define JIMP_EXERCISES_ARRAYFILL_H
+#include <vector>
+namespace arrays {
+    class ArrayFill {
+    public:
+        virtual int Value(int index) const =0;
+    };
 
+    void FillArray(std::size_t size, const ArrayFill &filler, std::vector<int> *v);
 
-class ArrayFill {
+    class UniformFill : public ArrayFill {
+    public:
+        UniformFill(int value = 0) : value_{value} {}
+        virtual int Value(int index) const override;
+    private:
+        int value_;
+    };
 
-};
-
+}
 
 #endif //JIMP_EXERCISES_ARRAYFILL_H
