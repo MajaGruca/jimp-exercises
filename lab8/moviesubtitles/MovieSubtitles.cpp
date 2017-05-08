03a11caa1372394d54cf00ddd9db73aa7faeb441
PaulinaKaplonJimp <paulinakaplon@o2.pl>
2017-05-08 18:11:37 +0200

    popdzielone na dwie funkcje

--- lab8/moviesubtitles/MovieSubtitles.cpp
+++ lab8/moviesubtitles/MovieSubtitles.cpp
@@ -18,19 +18,8 @@ namespace moviesubs {
     }
 
     void MicroDvdSubtitles::ShiftAllSubtitlesBy(int delay, int fps, std::stringstream *in, std::stringstream *out) {
-        MovieSubtitles::ShiftAllSubtitlesBy(delay, fps, in, out);
-    }
+        //MovieSubtitles::ShiftAllSubtitlesBy(delay, fps, in, out);
 
-
-    MovieSubtitles::MovieSubtitles() {
-
-    }
-
-    MovieSubtitles::~MovieSubtitles() {
-
-    }
-
-    void MovieSubtitles::ShiftAllSubtitlesBy(int delay, int fps, std::stringstream *in, std::stringstream *out) {
         std::string pom = in->str();
         double dodanie_o = ((double) delay / 1000) * fps;
         std::smatch mm;
@@ -47,8 +36,6 @@ namespace moviesubs {
             std::size_t pos = pom.find("\n");
             line_num++;
 
-            // MovieSubtitlesError::Get_line(line_num);
-            //SubtitleEndBeforeStart::SetLine(line_num);
             if (pos > pom.length()) {
                 pos = pom.length();
             }
@@ -71,47 +58,24 @@ namespace moviesubs {
                                            "{" + std::to_string(tab_pom[0]) + "}{" + std::to_string(tab_pom[1]) + "}")
                      << '\n';
             } else {
-                if (std::regex_match(linijka, std::regex(
-                        "([0-9]{2}:){2}([0-9]{2},[0-9]{3})([[:space:]]-->[[:space:]])([0-9]{2}:){2}([0-9]{2},[0-9]{3})"))) {
-                    linijka_pom = linijka;
-                    for (int j = 0; j < 8; j++) {
-                        std::regex_search(linijka_pom, mm, ee);
-                        for (auto x:mm) tab_pom1[j] = x;
-                        linijka_pom = mm.suffix().str();
-                    }
-                    int t = 3;
-                    for (int a = 0; a < 2; a++) {
-                        if (a == 1) t = 7;
-                        tab_pom1[t] = std::to_string(stoi(tab_pom1[t]) + delay);
-                        if (stoi(tab_pom1[t]) / 1000) {
-                            tab_pom1[t - 1] = std::to_string(stoi(tab_pom1[t - 1]) + stoi(tab_pom1[t]) / 1000);
-                            tab_pom1[t] = std::to_string(stoi(tab_pom1[t]) % 1000);
-                            if (stoi(tab_pom1[t - 1]) / 60) {
-                                tab_pom1[t - 2] = std::to_string(stoi(tab_pom1[t - 2]) + stoi(tab_pom1[t - 1]) / 60);
-                                tab_pom1[t - 1] = std::to_string(stoi(tab_pom1[t - 1]) % 60);
-                                if (stoi(tab_pom1[t - 2]) / 60) {
-                                    tab_pom1[t - 3] = std::to_string(
-                                            stoi(tab_pom1[t - 3]) + stoi(tab_pom1[t - 2]) / 60);
-                                    tab_pom1[t - 2] = std::to_string(stoi(tab_pom1[t - 2]) % 60);
-                                }
-                            }
-                        }
-                        while (tab_pom1[t].length() != 3) {
-                            tab_pom1[t] = "0" + tab_pom1[t];
-                        }
-                        for (int j = 0; j < 3; j++) {
-                            if (tab_pom1[t - j - 1].length() != 2)
-                                tab_pom1[t - j - 1] = "0" + tab_pom1[t - j - 1];
-                        }
-                    }
-                    *out << std::regex_replace(linijka, std::regex(
-                            "([0-9]{2}:){2}([0-9]{2},[0-9]{3})([[:space:]]-->[[:space:]])([0-9]{2}:){2}([0-9]{2},[0-9]{3})"),
-                                               tab_pom1[0] + ":" + tab_pom1[1] + ":" + tab_pom1[2] + "," + tab_pom1[3] +
-                                               " --> " + tab_pom1[4] + ":" + tab_pom1[5] + ":" + tab_pom1[6] + "," +
-                                               tab_pom1[7]) << '\n';
-                } else *out << linijka << '\n';
+                throw InvalidSubtitleLineFormat();
             }
         }
+
+
+    }
+
+
+    MovieSubtitles::MovieSubtitles() {
+
+    }
+
+    MovieSubtitles::~MovieSubtitles() {
+
+    }
+
+    void MovieSubtitles::ShiftAllSubtitlesBy(int delay, int fps, std::stringstream *in, std::stringstream *out) {
+
     }
 
     SubRipSubtitles::SubRipSubtitles() {
@@ -123,7 +87,70 @@ namespace moviesubs {
     }
 
     void SubRipSubtitles::ShiftAllSubtitlesBy(int delay, int fps, std::stringstream *in, std::stringstream *out) {
-        MovieSubtitles::ShiftAllSubtitlesBy(delay, fps, in, out);
+
+        std::string pom = in->str();
+        std::smatch mm;
+        std::smatch mmm;
+        std::regex ee("[0-9]+");
+        //std::regex eee("([0-9]{2}:){2}([0-9]{2},[0-9]{3})");
+        std::string linijka_pom;
+        std::string temp;
+        std::string tab_pom1[8];
+        int line_num = 0;
+
+        while (pom.length() > 0) {
+            std::size_t pos = pom.find("\n");
+            line_num++;
+
+            if (pos > pom.length()) {
+                pos = pom.length();
+            }
+            std::string linijka = pom.substr(0, pos);
+            pom = pom.erase(0, pos + 1);
+            //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+            if (std::regex_match(linijka, std::regex("([0-9]{2}:){2}([0-9]{2},[0-9]{3})([[:space:]]-->[[:space:]])([0-9]{2}:){2}([0-9]{2},[0-9]{3})"))) {
+                linijka_pom = linijka;
+                for (int j = 0; j < 8; j++) {
+                    std::regex_search(linijka_pom, mm, ee);
+                    for (auto x:mm) tab_pom1[j] = x;
+                    linijka_pom = mm.suffix().str();
+                }
+                int t = 3;
+                for (int a = 0; a < 2; a++) {
+                    if (a == 1) t = 7;
+                    tab_pom1[t] = std::to_string(stoi(tab_pom1[t]) + delay);
+                    if (stoi(tab_pom1[t]) / 1000) {
+                        tab_pom1[t - 1] = std::to_string(stoi(tab_pom1[t - 1]) + stoi(tab_pom1[t]) / 1000);
+                        tab_pom1[t] = std::to_string(stoi(tab_pom1[t]) % 1000);
+                        if (stoi(tab_pom1[t - 1]) / 60) {
+                            tab_pom1[t - 2] = std::to_string(stoi(tab_pom1[t - 2]) + stoi(tab_pom1[t - 1]) / 60);
+                            tab_pom1[t - 1] = std::to_string(stoi(tab_pom1[t - 1]) % 60);
+                            if (stoi(tab_pom1[t - 2]) / 60) {
+                                tab_pom1[t - 3] = std::to_string(
+                                        stoi(tab_pom1[t - 3]) + stoi(tab_pom1[t - 2]) / 60);
+                                tab_pom1[t - 2] = std::to_string(stoi(tab_pom1[t - 2]) % 60);
+                            }
+                        }
+                    }
+                    while (tab_pom1[t].length() != 3) {
+                        tab_pom1[t] = "0" + tab_pom1[t];
+                    }
+                    for (int j = 0; j < 3; j++) {
+                        if (tab_pom1[t - j - 1].length() != 2)
+                            tab_pom1[t - j - 1] = "0" + tab_pom1[t - j - 1];
+                    }
+                }
+                *out << std::regex_replace(linijka, std::regex("([0-9]{2}:){2}([0-9]{2},[0-9]{3})([[:space:]]-->[[:space:]])([0-9]{2}:){2}([0-9]{2},[0-9]{3})"),
+                                           tab_pom1[0] + ":" + tab_pom1[1] + ":" + tab_pom1[2] + "," + tab_pom1[3] +
+                                               " --> " + tab_pom1[4] + ":" + tab_pom1[5] + ":" + tab_pom1[6] + "," +
+                                               tab_pom1[7]) << '\n';
+            } else *out << linijka << '\n';
+
+        }
+
+
+
     }
 
     NegativeFrameAfterShift::NegativeFrameAfterShift() : MovieSubtitlesError("NegativeFrameAfterShift") {
