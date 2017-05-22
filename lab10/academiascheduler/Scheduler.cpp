63a1b0e91e5ab2465790c8374aa222c157769258
PaulinaKaplonJimp <paulinakaplon@o2.pl>
2017-05-22 21:29:18 +0200

    lab10

--- lab10/academiascheduler/Scheduler.cpp
+++ lab10/academiascheduler/Scheduler.cpp
@@ -5,10 +5,62 @@
 #include "Scheduler.h"
 
 
-
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
 
 namespace academia
 {
 
 
+    SchedulingItem::SchedulingItem(int new_course_id, int new_teacher_id, int new_room_id, int new_time_slot, int new_year) {
+
+        course_id=new_course_id;
+        teacher_id=new_teacher_id;
+        room_id=new_room_id;
+        time_slot=new_time_slot;
+        year=new_year;
+
+    }
+
+    Schedule Schedule::OfTeacher(int teacher_id) const {
+        Schedule temp;
+        std::copy_if (cos.begin(), cos.end(), std::back_inserter(temp.cos), [teacher_id](SchedulingItem it ){return teacher_id==it.TeacherId();} );
+        return temp;
+    }
+
+    Schedule Schedule::OfRoom(int room_id) const {
+        Schedule temp;
+        std::copy_if (cos.begin(), cos.end(), std::back_inserter(temp.cos), [room_id](SchedulingItem it ){return room_id==it.RoomId();} );
+        return temp;
+    }
+
+    Schedule Schedule::OfYear(int year) const {
+        Schedule temp;
+        std::copy_if (this->cos.begin(), this->cos.end(), std::back_inserter(temp.cos), [year](SchedulingItem it ){return (year==it.Year());} );
+        return temp;
+    }
+
+    std::vector<int> Schedule::AvailableTimeSlots(int n_time_slots) const {
+        return std::vector<int>();
+    }
+
+    void Schedule::InsertScheduleItem(const SchedulingItem &item) {
+        cos.push_back(item);
+    }
+
+    std::size_t Schedule::Size() const {
+        return cos.size();
+    }
+
+    SchedulingItem Schedule::operator[](int it) const {
+        return cos[it];
+    }
+
+    Schedule Scheduler::PrepareNewSchedule(const std::vector<int> &rooms,
+                                           const std::map<int, std::vector<int>> &teacher_courses_assignment,
+                                           const std::map<int, std::set<int>> &courses_of_year, int n_time_slots) {
+        return Schedule();
+    }
 }
--- lab10/academiascheduler/Scheduler.h
+++ lab10/academiascheduler/Scheduler.h
@@ -10,10 +10,31 @@
 #include <map>
 
 namespace academia
-{
-    class Scheduler {
+{  class SchedulingItem{
+        /*course_id - identyfikator kursu
+        teacher_id - identyfikator nauczyciela akademickiego prowadzącego te zajęcia o tej godzinie
+        room_id - identyfikator pomieszczenia w którym odbywają się zajęcia
+        time_slot - okno czasowe, w którym odbywają się zajęcia, dla uproszczenia cały tydzień jest podzielony na N okien czasowych nie zachodzących na siebie (np. dla 20: 1 ⇒ pon 8:00-10:00, 2 ⇒ pon 10:00-12:00, … 20 ⇒ pią 14:00-16:00). Zajęcia wymagają zawsze pojedynczego okna czasowego i zawsze są z nim wyrównane tzn. nie mogą zajmować połowę 12ego i połowę 13ego
+        year - rok studiów na którym obowiązuje przedmiot
+         */
+
     public:
-        Schedule PrepareNewSchedule(const std::vector<int> &rooms, const std::map<int, std::vector<int>> &teacher_courses_assignment, const std::map<int, std::set<int>> &courses_of_year, int n_time_slots);
+
+        SchedulingItem(int course_id,int teacher_id,int room_id,int time_slot,int year);
+        ~SchedulingItem(){};
+        int CourseId() const { return course_id;};
+        int TeacherId() const { return teacher_id;};
+        int RoomId() const { return room_id;};
+        int TimeSlot() const { return time_slot;};
+        int Year() const { return year;};
+
+    private:
+        int course_id;
+        int teacher_id;
+        int room_id;
+        int time_slot;
+        int year;
+
     };
     class Schedule
     {
@@ -25,6 +46,8 @@ void InsertScheduleItem(const SchedulingItem &item) - wstawia nowy element planu
 size_t Size() const - zwaraca rozmiar planu*/
     public:
 
+        Schedule(){};
+        ~Schedule(){};
         Schedule OfTeacher(int teacher_id) const;
         Schedule OfRoom(int room_id) const;
         Schedule OfYear(int year) const;
@@ -32,27 +55,22 @@ size_t Size() const - zwaraca rozmiar planu*/
         void InsertScheduleItem(const SchedulingItem &item);
         std::size_t Size() const;
 
-    };
-    class SchedulingItem{
-        /*course_id - identyfikator kursu
-        teacher_id - identyfikator nauczyciela akademickiego prowadzącego te zajęcia o tej godzinie
-        room_id - identyfikator pomieszczenia w którym odbywają się zajęcia
-        time_slot - okno czasowe, w którym odbywają się zajęcia, dla uproszczenia cały tydzień jest podzielony na N okien czasowych nie zachodzących na siebie (np. dla 20: 1 ⇒ pon 8:00-10:00, 2 ⇒ pon 10:00-12:00, … 20 ⇒ pią 14:00-16:00). Zajęcia wymagają zawsze pojedynczego okna czasowego i zawsze są z nim wyrównane tzn. nie mogą zajmować połowę 12ego i połowę 13ego
-        year - rok studiów na którym obowiązuje przedmiot
-         */
-
-    public:
-        SchedulingItem(int course_id,int teacher_id,int room_id,int time_slot,int year);
+        SchedulingItem operator[](int it) const;
 
     private:
-        int course_id;
-        int teacher_id;
-        int room_id;
-        int time_slot;
-        int year;
-
+        std::vector<SchedulingItem> cos;
     };
+    class Scheduler {
+    public:
+        Scheduler(){};
+        ~Scheduler(){};
+        Schedule PrepareNewSchedule(const std::vector<int> &rooms, const std::map<int, std::vector<int>> &teacher_courses_assignment, const std::map<int, std::set<int>> &courses_of_year, int n_time_slots);
+    };
+
+
     class GreedyScheduler{
+        GreedyScheduler(){};
+        ~GreedyScheduler(){};
 
     };
     class NoViableSolutionFound{
