7c5fcec5811da5818a52448ee50a68f9c76c0ea8
PaulinaKaplonJimp <paulinakaplon@o2.pl>
2017-05-30 20:42:50 +0200

    lab12

--- lab12/tree/Tree.h
+++ lab12/tree/Tree.h
@@ -5,10 +5,103 @@
 #ifndef JIMP_EXERCISES_TREE_H
 #define JIMP_EXERCISES_TREE_H
 
-namespace tree{
-        class Tree {
+#include <cstddef>
+#include <initializer_list>
+#include <algorithm>
+#include <memory>
 
-        };
+namespace tree{
+    template <class Element>
+    class Tree {
+        public:
+
+            Tree(const Element &cos):size_(1),value_(cos){}
+            Tree():size_(1){}
+
+            bool operator<(Element one);
+            bool operator<(Tree one);
+
+            void Insert(const Element &e);
+            bool Find(const Element &e);
+            Element Value(){ return value_;};
+            size_t Depth(){
+                if(L_dziecko && P_dziecko)
+                    return 1+std::max(L_dziecko->Depth(),P_dziecko->Depth());
+                else{
+                    if(L_dziecko)
+                        return 1+L_dziecko->Depth();
+                    if(P_dziecko)
+                        return 1+P_dziecko->Depth();
+                    return 1;
+                }
+            };
+            size_t Size(){ return size_;};
+
+        private:
+
+            size_t size_;
+            Element value_;
+
+            std::unique_ptr<Tree> L_dziecko;
+            std::unique_ptr<Tree> P_dziecko;
+    };
+
+    template <class Element>
+    bool Tree<Element>::Find(const Element &e) {
+
+        if(this->value_==e)
+            return true;
+
+        if(this->value_<e)
+        {
+            if(this->L_dziecko)
+                this->L_dziecko->Find(e);
+            else
+                return false;
+
+        } else
+        {
+            if(this->P_dziecko)
+                this->P_dziecko->Find(e);
+            else
+                return false;
+        }
+        return false;
+    }
+
+    template <class Element>
+    void Tree<Element>::Insert(const Element &e) {
+
+        size_++;
+
+        if(this->value_<e)
+        {
+            if(!this->L_dziecko) {
+
+                this->L_dziecko = std::make_unique<Tree<Element>>(e);
+
+            } else
+            {this->L_dziecko->Insert(e);}
+
+        } else
+        {
+            if(!this->P_dziecko) {
+
+                this->P_dziecko = std::make_unique<Tree<Element>>(e);
+            }
+            else
+            {this->P_dziecko->Insert(e);}
+        }
+    }
+    template <class Element>
+    bool Tree<Element>::operator<(Element one) {
+        return this->value_<one;
+    }
+
+    template <class Element>
+    bool Tree<Element>::operator<(Tree one) {
+        return this->value_<one.value_;
+    }
 }
 
 
