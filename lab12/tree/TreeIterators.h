70a0f57098a1b9a155e465ed921144e8c6f6c786
PaulinaKaplonJimp <paulinakaplon@o2.pl>
2017-06-06 13:27:24 +0200

    lab 12 mamy drzewa

--- lab12/tree/TreeIterators.h
+++ lab12/tree/TreeIterators.h
@@ -66,6 +66,119 @@ namespace tree
     private:
         Tree<Element> *tree_;
     };
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+   template<class Element>
+    void SetVectorPreOrder(Tree<Element> *tree, std::vector<Tree<Element> *> &vec_in_or) {
+
+        vec_in_or.push_back(tree);
+        if (tree->L()) {
+            SetVectorPreOrder(tree->L(), vec_in_or);
+        }
+        if (tree->P()) {
+            SetVectorPreOrder(tree->P(), vec_in_or);
+        }
+    }
+
+    template<class Element>
+    class PreOrderTreeView;
+
+    template<class Element>
+    PreOrderTreeView<Element> PreOrder(Tree<Element> *tree);
+
+    template<class Element>
+    PreOrderTreeView<Element> PreOrder(Tree<Element> *tree) {
+        return PreOrderTreeView<Element>(tree);
+    }
+
+    template<class Element>
+    class PreOrderTreeIterator {
+    public:
+        PreOrderTreeIterator(Tree<Element> *tree) : iter_(0) {SetVectorPreOrder<Element>(tree, vec_in_or);}
+        PreOrderTreeIterator<Element> &operator++() {++iter_; return *this;}
+        Element operator*(){return vec_in_or[iter_]->Value();}
+        bool operator!=(const PreOrderTreeIterator<Element> &one) const{return iter_ != one.iter_;}
+        friend class PreOrderTreeView<Element>;
+
+    private:
+        void SetToEnd(){iter_ = vec_in_or.size();}
+        std::vector<Tree<Element> *> vec_in_or;
+        unsigned long iter_;
+    };
+
+    template<class Element>
+    class PreOrderTreeView {
+    public:
+        PreOrderTreeView(Tree<Element> *tree) : tree_(tree) {};
+        PreOrderTreeIterator<Element> begin(){
+            PreOrderTreeIterator<Element> new_iterator(tree_->Root());
+            return new_iterator;
+        }
+        PreOrderTreeIterator<Element> end(){
+            PreOrderTreeIterator<Element> new_iterator(tree_->Root());
+            new_iterator.SetToEnd();
+            return new_iterator;
+        }
+
+    private:
+        Tree<Element> *tree_;
+    };
+//////////////////////////////////////////////////////////////////////////////////////////////////
+    template<class Element>
+    void SetVectorPostOrder(Tree<Element> *tree, std::vector<Tree<Element> *> &vec_in_or) {
+        if (tree->L()) {
+            SetVectorPostOrder(tree->L(), vec_in_or);
+        }
+        if (tree->P()) {
+            SetVectorPostOrder(tree->P(), vec_in_or);
+        }
+        vec_in_or.push_back(tree);
+    }
+
+    template<class Element>
+    class PostOrderTreeView;
+
+    template<class Element>
+    PostOrderTreeView<Element> PostOrder(Tree<Element> *tree);
+
+    template<class Element>
+    PostOrderTreeView<Element> PostOrder(Tree<Element> *tree) {
+        return PostOrderTreeView<Element>(tree);
+    }
+
+    template<class Element>
+    class PostOrderTreeIterator {
+    public:
+        PostOrderTreeIterator(Tree<Element> *tree) : iter_(0) {SetVectorPostOrder<Element>(tree, vec_in_or);}
+        PostOrderTreeIterator<Element> &operator++() {++iter_; return *this;}
+        Element operator*(){return vec_in_or[iter_]->Value();}
+        bool operator!=(const PostOrderTreeIterator<Element> &one) const{return iter_ != one.iter_;}
+        friend class PostOrderTreeView<Element>;
+
+    private:
+        void SetToEnd(){iter_ = vec_in_or.size();}
+        std::vector<Tree<Element> *> vec_in_or;
+        unsigned long iter_;
+    };
+
+    template<class Element>
+    class PostOrderTreeView {
+    public:
+        PostOrderTreeView(Tree<Element> *tree) : tree_(tree) {};
+        PostOrderTreeIterator<Element> begin(){
+            PostOrderTreeIterator<Element> new_iterator(tree_->Root());
+            return new_iterator;
+        }
+        PostOrderTreeIterator<Element> end(){
+            PostOrderTreeIterator<Element> new_iterator(tree_->Root());
+            new_iterator.SetToEnd();
+            return new_iterator;
+        }
+
+    private:
+        Tree<Element> *tree_;
+    };
+
+
 
 }
 
