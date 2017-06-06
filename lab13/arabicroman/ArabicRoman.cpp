e27b6c9e0d2e643c2841f706607bcca51e7348fd
PaulinaKaplonJimp <paulinakaplon@o2.pl>
2017-06-06 18:50:15 +0200

    lab13

---
+++ lab13/arabicroman/ArabicRoman.cpp
@@ -0,0 +1,13 @@
+//
+// Created by ktr on 06.06.2017.
+//
+
+#include "ArabicRoman.h"
+
+int RomanToArabic(std::string a) {
+    return 0;
+}
+
+std::string ArabicToRoman(int a) {
+    return "nic";
+}
